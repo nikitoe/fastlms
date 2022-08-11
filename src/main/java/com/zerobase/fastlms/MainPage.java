1a28cb0d8b308a2ad2e91ddc8666a359479b426b
Jiyong Sim <sjy19910222@gmail.com>
2022-08-12 00:17:32 +0900

    05-깃클론과 esponse객체를 이용한 HTML작성

--- src/main/java/com/zerobase/fastlms/MainPage.java
+++ src/main/java/com/zerobase/fastlms/MainPage.java
@@ -1,9 +1,15 @@
 package com.zerobase.fastlms;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@Controller
 public class MainPage {
 
     @RequestMapping("/")
@@ -12,7 +18,24 @@ public class MainPage {
     }
 
     @RequestMapping("/hello")
-    public String hello() {
-        return "Hello Page";
+    public void hello(HttpServletRequest request,
+                        HttpServletResponse response) throws IOException {
+
+        response.setContentType("text/html;charset=UTF-8");
+        PrintWriter printWriter = response.getWriter();
+
+        String msg = "<html>" +
+                "<head>" +
+                "<meta charset='UTF-8'>" +
+                "</head>" +
+                "<body>" +
+                "<p>hello</p>" +
+                "<p>fastlms webs!</p>" +
+                "<p>안녕하세요</p>" +
+                "</body>" +
+                "</html>";
+
+        printWriter.write(msg);
+        printWriter.close();
     }
 }
