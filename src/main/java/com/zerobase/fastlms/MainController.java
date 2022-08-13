bd86b9da67ac26c8a20a866c609cadf2b049fcb7
Jiyong Sim <sjy19910222@gmail.com>
2022-08-13 12:46:00 +0900

    10-JavaMailSender를 통한 메일 전송 설정 및 메일 전송 테스트 (Gmail이용)

--- src/main/java/com/zerobase/fastlms/MainController.java
+++ src/main/java/com/zerobase/fastlms/MainController.java
@@ -1,42 +1,27 @@
 package com.zerobase.fastlms;
 
 
-import com.zerobase.fastlms.member.repository.MemberRepository;
+import com.zerobase.fastlms.components.MailComponents;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
-import java.io.PrintWriter;
-
 @Controller
+@RequiredArgsConstructor
 public class MainController {
+
+    private final MailComponents mailComponents;
 
     @RequestMapping("/")
     public String index() {
+
+        String email = "simzzangn1@naver.com";
+        String subject = "안녕하세요. 지투더용 입니다.";
+        String text = "<p>반갑습니다.</p>";
+
+        mailComponents.sendMail(email, subject, text);
+
         return "index";
     }
 
-    @RequestMapping("/hello")
-    public void hello(HttpServletRequest request,
-                        HttpServletResponse response) throws IOException {
-
-        response.setContentType("text/html;charset=UTF-8");
-        PrintWriter printWriter = response.getWriter();
-
-        String msg = "<html>" +
-                "<head>" +
-                "<meta charset='UTF-8'>" +
-                "</head>" +
-                "<body>" +
-                "<p>hello</p>" +
-                "<p>fastlms webs!</p>" +
-                "<p>안녕하세요</p>" +
-                "</body>" +
-                "</html>";
-
-        printWriter.write(msg);
-        printWriter.close();
-    }
 }
