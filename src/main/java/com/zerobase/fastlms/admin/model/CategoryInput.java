ac66670e06b36347be5c44a6d321302a02f3498c
Jiyong Sim <sjy19910222@gmail.com>
2022-08-18 03:04:43 +0900

    09-강좌 카테고리 수정, 삭제, 정렬

--- src/main/java/com/zerobase/fastlms/admin/model/CategoryInput.java
+++ src/main/java/com/zerobase/fastlms/admin/model/CategoryInput.java
@@ -5,5 +5,9 @@ import lombok.Data;
 @Data
 public class CategoryInput {
 
+    long id;
     String categoryName;
+    int sortValue;
+    boolean usingYn;
+
 }
--- src/main/java/com/zerobase/fastlms/admin/service/CategoryServiceImpl.java
+++ src/main/java/com/zerobase/fastlms/admin/service/CategoryServiceImpl.java
@@ -2,22 +2,31 @@ package com.zerobase.fastlms.admin.service;
 
 import com.zerobase.fastlms.admin.dto.CategoryDto;
 import com.zerobase.fastlms.admin.entity.Category;
+import com.zerobase.fastlms.admin.model.CategoryInput;
 import com.zerobase.fastlms.admin.repository.CategoryRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 public class CategoryServiceImpl implements CategoryService {
 
-    final private CategoryRepository categoryRepository;
+    private final  CategoryRepository categoryRepository;
+    private Sort getSortBySortValueDesc(){
+
+        return  Sort.by(Sort.Direction.DESC, "sortValue");
+    }
+
+
 
     @Override
     public List<CategoryDto> list() {
 
-        List<Category> categories = categoryRepository.findAll();
+        List<Category> categories = categoryRepository.findAll(getSortBySortValueDesc());
 
         return CategoryDto.of(categories);
     }
@@ -39,12 +48,28 @@ public class CategoryServiceImpl implements CategoryService {
     }
 
     @Override
-    public boolean update(CategoryDto parameter) {
-        return false;
+    public boolean update(CategoryInput parameter) {
+
+        Optional<Category> optionalCategory = categoryRepository.findById(parameter.getId());
+        if (optionalCategory.isPresent()) {
+
+            Category category = optionalCategory.get();
+
+            category.setCategoryName(parameter.getCategoryName());
+            category.setSortValue(parameter.getSortValue());
+            category.setUsingYn(parameter.isUsingYn());
+
+            categoryRepository.save(category);
+        }
+
+        return true;
     }
 
     @Override
     public boolean del(long id) {
-        return false;
+
+        categoryRepository.deleteById(id);
+
+        return true;
     }
 }
