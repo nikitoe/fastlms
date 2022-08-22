4035ce0be110c1441095d6151c1b84fb28e59b97
Jiyong Sim <sjy19910222@gmail.com>
2022-08-22 15:31:00 +0900

    04_백오피스 - 강좌 신청 처리 구현

--- src/main/java/com/zerobase/fastlms/course/model/ServiceResult.java
+++ src/main/java/com/zerobase/fastlms/course/model/ServiceResult.java
@@ -1,11 +1,22 @@
 package com.zerobase.fastlms.course.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
 public class ServiceResult {
 
     boolean result;
     String message;
 
+    public ServiceResult(boolean result, String message) {
+        this.result = result;
+        this.message = message;
+    }
+
+    public ServiceResult(boolean result) {
+        this.result = result;
+    }
 }
